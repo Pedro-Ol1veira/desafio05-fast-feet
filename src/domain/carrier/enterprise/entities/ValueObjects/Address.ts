a1c2f53c3541ec22ee1369a5f0c3847d2889b85c
Pedro-Ol1veira <pedru.oliveira865@gmail.com>
2026-04-10 17:20:33 -0300

    Refactor: Adiciona getters para o value object Address

--- src/domain/carrier/enterprise/entities/ValueObjects/Address.ts
+++ src/domain/carrier/enterprise/entities/ValueObjects/Address.ts
@@ -10,6 +10,24 @@ export interface AddressProps {
 
 export class Address extends ValueObject<AddressProps> {
     
+    get latitude() {
+        return this.props.latitude;
+    }
+    get longitude() {
+        return this.props.longitude;
+    }
+    get street() {
+        return this.props.street;
+    }
+    get number() {
+        return this.props.number;
+    }
+    get complement() {
+        return this.props.complement;
+    }
+
+    
+    
     static create(props: AddressProps) {
         const address = new Address(props);
         return address;
